bbc159499bfbd7b5e3a75e1cbcf8a825448abf3c
Manikandan <manipvi20@gmail.com>
2017-11-10 10:46:23 -0800

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
 
   entry: {
     'app': './src/main.ts',
+    'vendor': './src/vendor.ts',
     'polyfills': [
       'core-js/es6',
       'core-js/es7/reflect',
@@ -20,21 +21,26 @@ module.exports = {
       {test: /\.component.ts$/, loader: 'ts!angular2-template'},
       {test: /\.ts$/, exclude: /\.component.ts$/, loader: 'ts'},
       {test: /\.html$/, loader: 'raw'},
-      {test: /\.(jpe?g|jpg|png|gif|svg)$/i, loader: "file-loader?name=/public/icons/[name].[ext]"},
-      {
+     // {test: /\.(jpe?g|jpg|png|gif|svg)$/i, loader: "file-loader?name=/public/icons/[name].[ext]"},
+     /* {
           test: /\.(woff|woff2|eot|ttf|svg)$/,
           exclude: /node_modules/,
           loader: 'file-loader?limit=1024&name=assets.fonts/[name]'
-      },
-      {test: /\.css$/, loader: 'raw'}
+      },*/
+     {test: /\.css$/, loader: 'raw'}
+     //{test: /\css$/, loader: 'style!css!file-loader?name=assets/css/common.css', options: { minimize: true } }      
     ]
   },
   resolve: {
     extensions: ['', '.js', '.ts', '.html', '.css']
   },
   plugins: [
+    new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+    }),
     new webpack.optimize.CommonsChunkPlugin({
-      name: 'polyfills'
+      name: ['polyfills', 'vendor']
     }),
     new HtmlWebpackPlugin({
       template: './src/index.html'
